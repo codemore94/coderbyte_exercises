40d5d7578b3268bc656384741bd72358efd35cd7
Mikko Saari <mikko.l.saari@tuni.fi>
2024-07-14 14:02:58 +0300

    developing_suitable_input_processing

--- is_hamilonian.cpp
+++ is_hamilonian.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 using namespace std;
 
 string HamiltonianPath(string strArr[], int arrLength) {
   
   // code goes here  
   vector<char>vertices;
-  
+  map<char,char>edges;
+  map<char,char>edges2;  
+  char latest_char ='?';
+
+  bool is_char = false;
   bool in_dot = false;
   bool block_begins = false;
   bool block_ends = false;
@@ -23,11 +28,12 @@ string HamiltonianPath(string strArr[], int arrLength) {
         //cout<< strArr[i][j] << endl;
         if(strArr[i][j] == ','){
           in_dot = true;
-          block_begins = false;
+          //block_begins = false;
           block_ends = false;
           left_par = false;
           right_par = false;
           is_line = false;
+          is_char = false;
         }
         else if(strArr[i][j] == '('){
           in_dot = false;
@@ -36,6 +42,7 @@ string HamiltonianPath(string strArr[], int arrLength) {
           left_par = true;
           right_par = false;
           is_line = false;
+          is_char = false;
         }
         else if(strArr[i][j] == ')'){
           in_dot = false;
@@ -44,16 +51,21 @@ string HamiltonianPath(string strArr[], int arrLength) {
           left_par = false;
           right_par = true;
           is_line = false;
+          is_char = false;
         }
 
         else{
 //          cout<< strArr[i][j] << endl;
           in_dot = false;
-          block_begins = false;
+          //block_begins = false;
           block_ends = false;
           left_par = false;
           right_par = false;
+          is_char = true;
+          //latest_char = strArr[i][j];
           vertices.push_back(strArr[i][j]);
+          //edges.insert(make_pair(latest_char,strArr[i][j]));
+          edges2.insert(make_pair(strArr[i][j],latest_char));
         }
         ++j;
       }
@@ -62,12 +74,15 @@ string HamiltonianPath(string strArr[], int arrLength) {
       int j = 0;
       while(j<strArr[i].size()){
         //cout<< strArr[i][j] << endl;
+        //latest_char = strArr[i][j];
+        //cout<< "In i" << i<< " " <<" in j" << j << " " << latest_char << endl;
         if(strArr[i][j] == ','){
           in_dot = true;
           block_begins = false;
           block_ends = false;
           left_par = false;
           right_par = false;
+          is_char = false;
         }
         else if(strArr[i][j] == '('){
           in_dot = false;
@@ -75,6 +90,7 @@ string HamiltonianPath(string strArr[], int arrLength) {
           block_ends = false;
           left_par = true;
           right_par = false;
+          is_char = false;
         }
         else if(strArr[i][j] == ')'){
           in_dot = false;
@@ -82,6 +98,7 @@ string HamiltonianPath(string strArr[], int arrLength) {
           block_ends = true;
           left_par = false;
           right_par = true;
+          is_char = false;
         }
         else if(strArr[i][j] == '-'){
           in_dot = false;
@@ -90,20 +107,40 @@ string HamiltonianPath(string strArr[], int arrLength) {
           left_par = false;
           right_par = false;
           is_line = true;
+          is_char = false;
+          
+          //vertices.insert(make_pair(strArr[i][j],latest_char));
         }
 
         else{
-          cout<< strArr[i][j] << endl;
+          //cout<< strArr[i][j] << endl;
           in_dot = false;
           block_begins = false;
           block_ends = false;
           left_par = false;
           right_par = false;
-          vertices.push_back(strArr[i][j]);
+          is_char = true;
+          
+          latest_char = strArr[i][j];
+          edges2.insert(make_pair(strArr[i][j],latest_char)); 
+          //vertices.push_back(strArr[i][j]);
         }
         ++j;
       }
     }
+  }
+
+  
+  for (auto vt:vertices){
+    //cout<< vt << endl;
+  }
+  /*for (auto mt:edges2){
+    cout<<"Herefirst" <<" "<<mt.first << endl;
+    cout<<"Theresec" <<" "<<mt.second << endl;
+  }*/
+  for (auto mt2:edges2){
+    cout<<"m!first " <<mt2.first << endl;
+    cout<<"m!sec " <<" "<<mt2.second << endl;
   }
 
   return strArr[0];
