5201f3f9d0bc3ed9ddce29ee84e480bbceaa797b
Mikko Saari <mikko.l.saari@tuni.fi>
2024-08-17 12:25:13 +0300

    nested_map

---
+++ assign4.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Boolean{
+  bool True=true;
+  bool False=false;
+};
+  vector<bool>booleans;
+  map<char,struct Boolean>propositions;
+  map<int,map<char,struct Boolean>>logics;
+  
+void evaluate(map<char,struct Boolean>& propositions){
+
+}
+
+
+string SimpleSAT(string str) {
+  int counter = 0;
+  Boolean boolean_values;
+  
+  for(int i=0;i<str.size();++i){
+    if(str.at(i) == '('){
+
+    }
+    else if(str.at(i) == '&'){
+
+    }
+    else if(str.at(i) == '|'){
+      
+    }
+    else if(str.at(i) == '~'){
+      
+    }
+    else if(isalpha(str.at(i))){
+      cout<< str.at(i) << endl;   
+      propositions.insert(make_pair(str.at(i),boolean_values));
+//      propositions.insert(make_pair(str.at(i),std::pair<bool,bool>{true,false})));
+      //propositions.insert(make_pair(str.at(i),make_pair(true,false)));
+    
+    logics.insert(make_pair(counter,propositions));
+    ++counter;
+    }
+  }
+
+  map<char,Boolean>::iterator it;
+  it = propositions.begin();
+  while(it!=propositions.end()){
+    cout<< "Char" << it->first << endl;
+    cout<< "values" << it->second.False << endl;
+    ++it;
+  }
+
+  evaluate(propositions);
+
+
+
+  return str;
+
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  cout << SimpleSAT(coderbyteInternalStdinFunction(stdin));
+  return 0;
+    
+}
