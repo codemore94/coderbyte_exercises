d202d383da9d2892866ad0e4adde93c93afed413
Mikko Saari <mikko.l.saari@tuni.fi>
2024-08-19 02:33:30 +0300

    weighted&&templates

---
+++ templates.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+template<typename T>
+
+std::vector<int>vec;
+
+int main(){
+  vec<T>.push_back(0);
+  //for(int i = 1;i<10;++i){
+  //vec.push_back(i);
+  // }
+}
